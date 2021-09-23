427f7fca57b5a5f4857824a2f9eee4c6c8c311dc
Sithi5 <ma.sithis@gmail.com>
2021-09-23 14:01:39 +0200

    [test]: Update tests
    Remove some woody test (or commenting) and add Famine subject test.

--- tests/test_multiples_infections.sh
+++ tests/test_multiples_infections.sh
@@ -1,18 +1,18 @@
-_GREEN=`tput setaf 2`
-_YELLOW=`tput setaf 3`
-_END=`tput sgr0`
+# _GREEN=`tput setaf 2`
+# _YELLOW=`tput setaf 3`
+# _END=`tput sgr0`
 
-# echo filename
-echo "\n\n\n\n${_YELLOW}$0 :${_END}\n\n"
+# # echo filename
+# echo "\n\n\n\n${_YELLOW}$0 :${_END}\n\n"
 
 
-# Multiples infections should fail
-gcc ./tests/test1.c -o test1
-echo "\n${_GREEN}Creating and Executing famine with normal binary...${_END}\n"
-./famine test1
-./famine
-echo "\n${_GREEN}Try infecting a second time...${_END}\n"
-./famine famine
+# # Multiples infections should fail
+# gcc ./tests/test1.c -o test1
+# echo "\n${_GREEN}Creating and Executing famine with normal binary...${_END}\n"
+# ./famine test1
+# ./famine
+# echo "\n${_GREEN}Try infecting a second time...${_END}\n"
+# ./famine famine
 
-rm test1
-rm famine
+# rm test1
+# rm famine
---
+++ tests/test_subject_64bits.sh
@@ -0,0 +1,36 @@
+_GREEN=`tput setaf 2`
+_YELLOW=`tput setaf 3`
+_END=`tput sgr0`
+
+# Echo filename
+echo "\n\n\n\n${_YELLOW}$0 :${_END}\n\n"
+
+# Test subject
+rm -rf /tmp/test/
+rm -rf /tmp/test2/
+mkdir /tmp/test/
+mkdir /tmp/test2/
+printf '%s\n%s\n%s\n\t%s\n\t%s\n%s\n' "#include <stdio.h>" "int main(void)" "{" "printf(\"Hello, World\n\");" "return 0;" "}" > ./tests/tmp.c
+gcc -m64 ./tests/tmp.c -o /tmp/test/sample
+rm ./tests/tmp.c
+ls -al /tmp/test
+/tmp/test/sample
+file /tmp/test/sample
+strings /tmp/test/sample | grep "mabouce"
+cp /bin/ls /tmp/test2/
+ls -al /tmp/test2
+file /tmp/test2/ls
+
+./Famine
+strings /tmp/test/sample | grep "mabouce"
+/tmp/test/sample
+strings /tmp/test2/ls | grep "mabouce"
+/tmp/test2/ls -la /tmp/test2/
+
+gcc -m64 ./tests/tmp.c -o /tmp/test/sample
+ls -al /tmp/test
+/tmp/test/sample
+file /tmp/test/sample
+strings /tmp/test/sample | grep "mabouce"
+/tmp/test2/ls -la /tmp/test2/
+strings /tmp/test/sample | grep "mabouce"
