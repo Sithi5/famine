c92c923a538159542814dd3bb41c48c38c61a9f5
Sithi5 <ma.sithis@gmail.com>
2021-09-23 15:56:09 +0200

    [test](test_subject): Improve test.

--- tests/test_subject_64bits.sh
+++ tests/test_subject_64bits.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -x
 _GREEN=`tput setaf 2`
 _YELLOW=`tput setaf 3`
 _END=`tput sgr0`
@@ -10,6 +12,7 @@ rm -rf /tmp/test/
 rm -rf /tmp/test2/
 mkdir /tmp/test/
 mkdir /tmp/test2/
+
 printf '%s\n%s\n%s\n\t%s\n\t%s\n%s\n' "#include <stdio.h>" "int main(void)" "{" "printf(\"Hello, World\n\");" "return 0;" "}" > ./tests/tmp.c
 gcc -m64 ./tests/tmp.c -o /tmp/test/sample
 rm ./tests/tmp.c
@@ -17,13 +20,16 @@ ls -al /tmp/test
 /tmp/test/sample
 file /tmp/test/sample
 strings /tmp/test/sample | grep "mabouce"
+
 cp /bin/ls /tmp/test2/
 ls -al /tmp/test2
 file /tmp/test2/ls
 
 ./Famine
+
 strings /tmp/test/sample | grep "mabouce"
 /tmp/test/sample
+
 strings /tmp/test2/ls | grep "mabouce"
 /tmp/test2/ls -la /tmp/test2/
 
